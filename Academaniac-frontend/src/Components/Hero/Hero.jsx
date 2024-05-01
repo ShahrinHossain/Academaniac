ba505a670ddd83d69439c3899b8a8b18303c6758
AbDhrubo <abubakorsiddiquedhrubo@gmail.com>
2024-05-01 11:09:33 +0600

    has a search bar

--- Academaniac-frontend/src/Components/Hero/Hero.jsx
+++ Academaniac-frontend/src/Components/Hero/Hero.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Hero.css";
+import SearchBar from "./SearchBar";
+import SearchResultsList from "./SearchResultsList";
 
 const Hero = () => {
+  const [results, setResults] = useState([]);
+
   return (
-    <div className="hero">
-      <h1>Hero</h1>
+    <div className="hero container">
+      <div className="hero-text">
+        <h1>Your Ultimate Study Abroad Guide</h1> <br />
+        <br />
+        <SearchBar setResults={setResults} />
+        <SearchResultsList results={results} />
+      </div>
     </div>
   );
 };

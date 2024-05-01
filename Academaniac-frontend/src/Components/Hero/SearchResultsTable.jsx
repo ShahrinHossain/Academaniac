a2c9a6e96891da0eb7660bf89e2639283f0bff86
AbDhrubo <abubakorsiddiquedhrubo@gmail.com>
2024-05-02 01:58:21 +0600

    added details page

--- Academaniac-frontend/src/Components/Hero/SearchResultsTable.jsx
+++ Academaniac-frontend/src/Components/Hero/SearchResultsTable.jsx
@@ -1,8 +1,51 @@
+// import React from "react";
+// import "./SearchResultsTable.css";
+// import { SearchResult } from "./SearchResult";
+
+// const SearchResultsTable = ({ results }) => {
+//   return (
+//     <div className="search-results-table">
+//       <h2>Search Results</h2>
+//       <table>
+//         <thead>
+//           <tr>
+//             <th>Name</th>
+//             <th>Country</th>
+//             <th>Action</th>
+//           </tr>
+//         </thead>
+//         <tbody>
+//           {results.map((result) => (
+//             <tr key={result.id}>
+//               <td>{result.name}</td>
+//               <td>{result.country}</td>
+//               <td>
+//                 <button
+//                   onClick={() => (
+//                     (window.location.href = result.web_pages[0]),
+//                     console.log(result.web_pages)
+//                   )}
+//                 >
+//                   View
+//                 </button>
+//               </td>
+//             </tr>
+//           ))}
+//         </tbody>
+//       </table>
+//     </div>
+//   );
+// };
+
+// export default SearchResultsTable;
+
 import React from "react";
 import "./SearchResultsTable.css";
-import {SearchResult} from "./SearchResult";
+import { useNavigate } from "react-router-dom";
 
 const SearchResultsTable = ({ results }) => {
+  const navigateTo = useNavigate(); // Get history object
+
   return (
     <div className="search-results-table">
       <h2>Search Results</h2>
@@ -20,7 +63,16 @@ const SearchResultsTable = ({ results }) => {
               <td>{result.name}</td>
               <td>{result.country}</td>
               <td>
-                <button onClick={() => console.log("View button clicked for:", result)}>View</button>
+                <button
+                  // onClick={() => (navigteTo(`/details`), console.log(result))}
+                  onClick={() =>
+                    navigateTo(`/details`, {
+                      state: { result },
+                    })
+                  }
+                >
+                  View
+                </button>
               </td>
             </tr>
           ))}
---
+++ Academaniac-frontend/src/Components/View/NoPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NoPage = () => {
+  return (
+    <div>
+      <h2>Error: 404</h2>
+      <p>page not found</p>
+    </div>
+  );
+};
+
+export default NoPage;

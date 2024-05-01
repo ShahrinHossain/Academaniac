b285e322a89f2175929a89950f03c8c5b3cf7050
AbDhrubo <abubakorsiddiquedhrubo@gmail.com>
2024-05-01 09:29:09 +0600

    has bg

--- Academaniac-frontend/src/Components/Navbar/Navbar.jsx
+++ Academaniac-frontend/src/Components/Navbar/Navbar.jsx
@@ -4,13 +4,15 @@ import logo from "../../assets/logo.png";
 
 export const Navbar = () => {
   return (
-    <nav>
+    <nav className="container">
       <img src={logo} alt="Academaniac Logo" className="logo" />
       <ul>
         <li>Home</li>
         <li>Programs</li>
         <li>About Us</li>
-        <li>Contact Us</li>
+        <li>
+          <button className="btn">Contact Us</button>
+        </li>
       </ul>
     </nav>
   );

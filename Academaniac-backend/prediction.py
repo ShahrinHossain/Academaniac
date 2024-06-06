c96caa8c148b6c24d8d0b9fb427352c1ae3ab83e
210041137 <abubakor@iut-dhaka.edu>
2024-06-06 12:40:38 +0600

    some apis added, db entry done

--- Academaniac-backend/prediction.py
+++ Academaniac-backend/prediction.py
@@ -1,18 +1,23 @@
 from flask import Blueprint, request, jsonify
 from prediction_package.get_prediction_cgpa import predict_admission
 import pandas as pd
+from models import db, University, User_Details, Department
 
 prediction = Blueprint("prediction", __name__, static_folder="static", template_folder="templates")
 
 
 @prediction.route('/', methods=['GET'])
 def get_prediction():
-
     data = request.json
-    rank = data.get('rank')
-    cgpa = data.get('cgpa')
-    dept = data.get('dept')
-    gre = data.get('gre')
+    user_id = data.get('user_id')
+    uni_id = data.get('uni_id')
+    program = data.get('program')
+
+    university = University.query.filter_by(id=uni_id).first()
+    user = User_Details.query.filter_by(id=user_id).first()
+    dept = Department.query.filter_by(name=program, uni_id=uni_id).first().mapped_id
+    rank = university.rank
+    cgpa = user.cgpa
 
     ans = round(predict_admission(university_rank=rank, department=dept, undergrad_cgpa=cgpa) * 100, 2)
 
@@ -21,7 +26,6 @@ def get_prediction():
 
 @prediction.route('/add', methods=['POST'])
 def add_data():
-
     data = request.json
     rank = data.get('rank')
     cgpa = data.get('cgpa')

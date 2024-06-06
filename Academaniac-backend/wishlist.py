cc486be283d202f420d8916bbd4dab96d63145d8
210041137 <abubakor@iut-dhaka.edu>
2024-06-06 16:43:52 +0600

    shesh?

--- Academaniac-backend/wishlist.py
+++ Academaniac-backend/wishlist.py
@@ -1,7 +1,9 @@
 from flask import Blueprint, request, session, jsonify
 from flask_cors import cross_origin, CORS
 from flask_session import Session
-from models import db, User, Department, Wishlist, University
+from models import db, User_Details, Department, Wishlist, University
+from prediction_package.get_prediction_cgpa import predict_admission
+
 
 wishlist = Blueprint("wishlist", __name__, static_folder="static", template_folder="templates")
 
@@ -35,11 +37,27 @@ def add_to_wishlist():
 def get_wishlist():
     if 'id' in session:
         user_id = session.get('id')
+        # user_id = 1
         wishes = Wishlist.query.filter_by(user_id=user_id).all()
+        user = User_Details.query.filter_by(id=user_id).first()  # Get the actual user instance
+        # print(user.name)
+        responses = []
+
+        for wish in wishes:
+            uni = University.query.filter_by(id=wish.uni_id).first()
+            dept = Department.query.filter_by(id=wish.program_id).first()
+            response = {}
+            response['university'] = uni.name
+            response['dept'] = dept.name
+            response['probability'] = round(
+                predict_admission(university_rank=uni.rank, department=dept.mapped_id, undergrad_cgpa=user.cgpa) * 100,
+                2)
+
+            responses.append(response)
 
         return jsonify({
-            "added": [{
-                "university": University.query.filter_by(id=wish.uni_id).first().name,
-                "dept": Department.query.filter_by(id=wish.program_id).first().name
-            } for wish in wishes]
+            "added": responses
         }), 201
+
+    return jsonify({"error": "User not logged in"}), 401
+

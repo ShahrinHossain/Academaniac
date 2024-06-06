0197184b14dcc25d2c266181d76d63a8c2c57e07
210041125_shahrinhossain <shahrin@iut-dhaka.edu>
2024-06-06 15:27:02 +0600

    SHesh

--- Academaniac-backend/wishlist.py
+++ Academaniac-backend/wishlist.py
@@ -1,39 +1,30 @@
 from flask import Blueprint, request, session, jsonify
-from flask_cors import cross_origin
+from flask_cors import cross_origin, CORS
 from flask_session import Session
 from models import db, User, Department, Wishlist, University
 
 wishlist = Blueprint("wishlist", __name__, static_folder="static", template_folder="templates")
 
 
-@cross_origin(supports_credentials=True, methods=['POST'])
-@wishlist.route('/', methods=['POST'])
+@cross_origin(supports_credentials=True, methods=['POST', 'OPTIONS'])
+@wishlist.route('/post', methods=['POST', 'OPTIONS'])
 def add_to_wishlist():
+    if request.method == 'OPTIONS':
+        # Handle preflight requests
+        return jsonify({"success": True}), 200
+
     data = request.json
     if 'id' in session:
         user_id = session.get('id')
-        # user_id = 1
         uni_rank = data.get('uni_rank')
         uni = University.query.filter_by(rank=uni_rank).first()
         dept_name = data.get('dept_name')
 
         dept = Department.query.filter_by(uni_id=uni.id, name=dept_name).first()
-        print(dept.name)
         wish = Wishlist(user_id=user_id, uni_id=uni.id, program_id=dept.id)
         db.session.add(wish)
         db.session.commit()
 
-        return jsonify(
-            {
-                "success": True
-            }
-        ), 201
-
+        return jsonify({"success": True}), 201
     else:
-        return jsonify({
-            "success": False,
-            "message": "login required"
-        }), 401
-
-
-
+        return jsonify({"success": False, "message": "login required"}), 401
